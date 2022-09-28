017102392a567b32f890177d2a38848370aea365
ItielSanz /AXO <l211080078@iztapalapa.tecnm.mx>
2022-09-28 15:52:25 -0500

    CODIGO FUENTE

---
+++ src/hola/mundo/HOLAMUNDO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author Lab2_00
+ */
+public class HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        pantalla vista= new pantalla();
+        vista.setVisible(true);
+        
+    }
+    
+}
