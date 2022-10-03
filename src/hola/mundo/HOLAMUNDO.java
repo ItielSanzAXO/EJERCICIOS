a120f4ee44ac78c4589f5eccc6c5fcd9c1828ad3
Hormiganster <114192995+Hormiganster10@users.noreply.github.com>
2022-10-02 22:30:19 -0500

    Update HOLAMUNDO.java
    Coloqué mi nombre.
    Eric Castillo

--- src/hola/mundo/HOLAMUNDO.java
+++ src/hola/mundo/HOLAMUNDO.java
@@ -32,6 +32,6 @@ public class HOLAMUNDO {
         System.out.println("Daniel");
         System.out.println("Ledesma!");
         System.out.println("José Luis");
-        
+         System.out.println("Eric Castillo. presente.");
 }
 }
