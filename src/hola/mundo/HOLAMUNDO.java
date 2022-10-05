4f8e0a75f446081577a709cfa67783d27c8e642b
ItielSanz /AXO <l211080078@iztapalapa.tecnm.mx>
2022-10-04 23:15:47 -0500

    AGREGUE LA CLASE MAIN DE NUEVO

---
+++ src/hola/mundo/HOLAMUNDO.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola.mundo;
+
+/**
+ *
+ * @author Lab2_00
+ */
+public class HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        pantalla vista= new pantalla();
+        vista.setVisible(true);
+        System.out.println("HOLA 1");
+        System.out.println("PRUBA 1");
+        System.out.println("PRUBA 2");
+        System.out.println("Itiel 1");
+        System.out.println("Pazos 2");
+        System.out.println("Pazos ESTUVO AQUI");
+        //COLOCA TU NOMBRE DESDE AQUI
+        System.out.println("Erik");
+        System.out.println("Gabriel");
+        System.out.println("Eunice");
+        System.out.println("Cristian");
+        System.out.println("Daniel");
+        System.out.println("Ledesma!");
+        System.out.println("José Luis");
+         System.out.println("Eric Castillo. presente.");
+        
+}
+}
