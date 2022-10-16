76e6216bffe869f57ac8dd48ae1ae6776d06e2d6
QuesilloOfic <l201080210@iztapalapa.tecnm.mx>
2022-10-15 22:09:17 -0500

    Agregue botón
    Se agrego Botón "CARGAR FOTO"

--- src/Ejercicio3_4/Jose.java
+++ src/Ejercicio3_4/Jose.java
@@ -6,6 +6,14 @@
 package Ejercicio3_4;
 
 import hola.mundo.CONTRASEÑA;
+import java.awt.Image;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
@@ -16,10 +24,38 @@ public class Jose extends javax.swing.JFrame {
     /**
      * Creates new form Jose
      */
+    FileInputStream fis;
+    int LongitudBytes;
     public Jose() {
         initComponents();
     }
-
+          public void CargarFoto(){
+        JFileChooser j=new JFileChooser();
+        FileNameExtensionFilter filtro= new FileNameExtensionFilter("JPG,PNG,JPEG", 
+                "jpg","png","jpeg");
+        j.setFileFilter(filtro);
+        
+        int estado=j.showOpenDialog(null);
+        
+        if(estado==JFileChooser.APPROVE_OPTION){
+            try {
+                fis=new FileInputStream(j.getSelectedFile());
+                this.LongitudBytes=(int)j.getSelectedFile().length();
+                try {
+                    lblFoto.setIcon(null);
+                    Image icono = ImageIO.read(j.getSelectedFile()).getScaledInstance(lblFoto.getWidth(), 
+                            lblFoto.getHeight(), Image.SCALE_DEFAULT);
+                    lblFoto.setIcon(new ImageIcon(icono));
+                    lblFoto.updateUI();
+                    System.out.println("LongitudBytes:"+LongitudBytes);
+                } catch (IOException e) {
+                    System.out.println("Error al cargar la foto IO: "+e);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("Error al cargar file: "+e);
+            }
+        }
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -30,21 +66,30 @@ public class Jose extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        lblFoto = new javax.swing.JLabel();
         txtNombre = new javax.swing.JTextField();
         txtVerificar = new javax.swing.JButton();
+        btnCargar = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(204, 0, 255));
 
-        jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        lblFoto.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         txtVerificar.setText("VALIDAR");
         txtVerificar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         txtVerificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtVerificarActionPerformed(evt);
+            }
+        });
+
+        btnCargar.setText("CARGAR FOTO");
+        btnCargar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnCargar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCargarActionPerformed(evt);
             }
         });
 
@@ -57,24 +102,30 @@ public class Jose extends javax.swing.JFrame {
                 .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(80, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(222, 222, 222)
-                .addComponent(txtVerificar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(195, 195, 195)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblFoto, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(txtVerificar)
+                .addGap(214, 214, 214))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(196, 196, 196)
+                .addComponent(btnCargar)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(lblFoto, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(46, 46, 46)
                 .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(52, 52, 52)
+                .addGap(31, 31, 31)
                 .addComponent(txtVerificar)
-                .addContainerGap(110, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(btnCargar)
+                .addContainerGap(81, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -100,6 +151,11 @@ public class Jose extends javax.swing.JFrame {
         vista.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_txtVerificarActionPerformed
+
+    private void btnCargarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCargarActionPerformed
+        // TODO add your handling code here:
+            CargarFoto();
+    }//GEN-LAST:event_btnCargarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -137,8 +193,9 @@ public class Jose extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JToggleButton btnCargar;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblFoto;
     private javax.swing.JTextField txtNombre;
     private javax.swing.JButton txtVerificar;
     // End of variables declaration//GEN-END:variables
