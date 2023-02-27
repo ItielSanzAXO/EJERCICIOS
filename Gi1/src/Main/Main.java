d23a1755d3b2730e6cfe5bfaa273c0492f921cc0
ItielSanz /AXO <113627289+ItielSanzAXO@users.noreply.github.com>
2023-02-27 07:35:07 -0600

    DELET1

--- Gi1/src/Main/Main.java
+++
@@ -1,9 +0,0 @@
-
-package Main;
-
-import Views.Principal;
-public class Main {
-    public static void main(String args[]){
-        new Principal().setVisible(true);
-    }
-}
--- Gi1/src/Models/Personal.java
+++
@@ -1,9 +0,0 @@
-
-package Models;
-
-public class Personal extends Itiz {
-    public String Departamento;
-    public int NumTarjeta;
-        
-    public Personal(){}
-}
